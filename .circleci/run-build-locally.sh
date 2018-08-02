daeb6537f992eba60315259b258175762f3d97d6
Haoqun Jiang <haoqunjiang@gmail.com>
2018-08-03 00:32:31 +0800

    update

--- .circleci/run-build-locally.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-curl --user ${CIRCLE_TOKEN}: \
-    --request POST \
-    --form revision=7a0273ce671cd933a835b9676630e90f1c43badf\
-    --form config=@config.yml \
-    --form notify=false \
-        https://circleci.com/api/v1.1/project/github/sodatea/regression-test-prototype/tree/master
